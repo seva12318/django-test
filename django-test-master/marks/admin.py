6e0413b03a1e605f40009db32eeeb8205b85ca2f
Vsevolod Sudomoykin <70789213+seva12318@users.noreply.github.com>
2022-07-03 12:35:53 +0800

    Add files via upload

---
+++ django-test-master/marks/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+
+from marks.models import Choice, Journal, Lesson, School, Student, Subject, Teacher
+
+# Register your models here.asdasda
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Journal)
+class JournalAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    pass
